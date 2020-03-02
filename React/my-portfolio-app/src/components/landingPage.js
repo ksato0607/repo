1ccfb10a3bfb56fb447912aad8a17239c1bad6a1
ksato <ksato@iastate.edu>
2020-03-02 22:39:27 +0900

    Landing page has been completed

--- React/my-portfolio-app/src/components/landingPage.js
+++ React/my-portfolio-app/src/components/landingPage.js
@@ -1,10 +1,58 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+              <p>
+                HTML/CSS | Bootstrap | Javascript | React | React Natve | NodeJS
+                | Express | MongoDB
+              </p>
+            </div>
+            <div className="social-links">
+              <a
+                href="https://google.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-linkedin-square" aria-hidden="true" />
+              </a>
+              <a
+                href="https://github.com/ksato0607"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-github-square" aria-hidden="true" />
+              </a>
+              <a
+                href="https://google.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-free-code-camp" aria-hidden="true" />
+              </a>
+              <a
+                href="https://google.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-youtube-square" aria-hidden="true" />
+              </a>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
