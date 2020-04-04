b29de103e4a6480d22d9d5715f58ee5c28f90459
ksato <ksato@iastate.edu>
2020-04-04 20:41:45 +0900

    Learned about store

--- React/redux-tutorial/src/index.js
+++ React/redux-tutorial/src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+import todoApp from "./app/reducers";
+// import {
+//   addTodo,
+//   toggleTodo,
+//   setVisibilityFilter,
+//   VisibilityFilters
+// } from './app/actions'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const store = createStore(todoApp);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // Log the initial state
+// console.log(store.getState())
+
+// // Every time the state changes, log it
+// // Note that subscribe() returns a function for unregistering the listener
+// const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// // Dispatch some actions
+// store.dispatch(addTodo('Learn about actions'))
+// store.dispatch(addTodo('Learn about reducers'))
+// store.dispatch(addTodo('Learn about store'))
+// store.dispatch(toggleTodo(0))
+// store.dispatch(toggleTodo(1))
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// // Stop listening to state updates
+// unsubscribe()
