e16570efa1261472437b99b987ad7178e5a365cf
ksato <ksato@iastate.edu>
2020-04-05 20:25:52 +0900

    Finished react redux todo list tutorial

--- React/redux-tutorial/src/index.js
+++ React/redux-tutorial/src/index.js
@@ -1,28 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
 import { createStore } from "redux";
-import todoApp from "./app/reducers";
-// import {
-//   addTodo,
-//   toggleTodo,
-//   setVisibilityFilter,
-//   VisibilityFilters
-// } from './app/actions'
+import { Provider } from "react-redux";
+import App from "./components/App";
+import rootReducer from "./reducers";
 
-const store = createStore(todoApp);
+const store = createStore(rootReducer);
 
-// // Log the initial state
-// console.log(store.getState())
-
-// // Every time the state changes, log it
-// // Note that subscribe() returns a function for unregistering the listener
-// const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-// // Dispatch some actions
-// store.dispatch(addTodo('Learn about actions'))
-// store.dispatch(addTodo('Learn about reducers'))
-// store.dispatch(addTodo('Learn about store'))
-// store.dispatch(toggleTodo(0))
-// store.dispatch(toggleTodo(1))
-// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-// // Stop listening to state updates
-// unsubscribe()
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
