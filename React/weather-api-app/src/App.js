6208cde46a78f6c38ec856e5956ad84971b38a8f
ksato <ksato@iastate.edu>
2020-07-09 14:53:37 +0900

    Finished a weather app

---
+++ React/weather-api-app/src/App.js
@@ -0,0 +1,63 @@
+/** 
+ * Follow this tutorial https://www.youtube.com/watch?v=GuA0_Z1llYU
+ */
+import React, { useState } from "react";
+import apiConfig from "./config";
+const moment = require("moment");
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(
+        `${apiConfig.base}weather?q=${query}&units=metric&APPID=${apiConfig.key}`
+      )
+        .then(response => response.json())
+        .then(result => {
+          setWeather(result);
+          setQuery("");
+        });
+    }
+  };
+
+  const isWarm = () => {
+    return weather.main && Math.round(weather.main.temp) > 15;
+  };
+  return (
+    <div className={isWarm() ? "app warm" : "app"}>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {weather.main ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{moment().format("MMM Do YYYY")}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        <div></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
