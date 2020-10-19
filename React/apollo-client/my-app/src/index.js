6fd44ad0b42917aa26eebffc4d0e666e2d253882
ksato <ksato@iastate.edu>
2020-10-19 20:20:27 +0900

    Learned about useQuery

--- React/apollo-client/my-app/src/index.js
+++ React/apollo-client/my-app/src/index.js
@@ -1,36 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { gql } from '@apollo/client';
+import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io/',
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <dir>
+        <h2>My first Apollo app 🚀</h2>
+        <ExchangeRates />
+      </dir>
+    </ApolloProvider>
+  );
+}
+
+render(<App />, document.getElementById('root'));
