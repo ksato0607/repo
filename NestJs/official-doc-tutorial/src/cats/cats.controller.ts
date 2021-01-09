75d242657c84ab0949e9c9ea3b2338a48eb8a607
Keisuke Sato <caseksato@gmail.com>
2021-01-09 19:07:01 +0900

    feat: started the nestjs official doc tutorial

---
+++ NestJs/official-doc-tutorial/src/cats/cats.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto): Promise<any> {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
